b050329924fa9a6071ee85d474e01638c2586327
Andreas Linz <klingt.net@gmail.com>
2014-11-18 16:02:57 +0100

    added parse method stub for sblgnt module

--- src/grarticle/sblgnt.py
+++ src/grarticle/sblgnt.py
@@ -1,3 +1,7 @@
+import os.path
+import logging
+log = logging.getLogger(__name__)
+
 '''
     A python module that parses texts in the format the SBLGNT uses
 '''
@@ -5,5 +9,17 @@
 def parse(filepath):
     '''
     '''
-    pass
+    if os.path.exists(filepath):
+        raw_text = ''
+        try:
+            with open(filepath) as f:
+                raw_text = f.read()
+        except IOError as e:
+            log.exception(e)
+        else:
+            if raw_text:
+                # to be done
+                return raw_text
+    else:
+        log.warn('Could not find file "{}"!'.format(filepath))
 
--- src/tests/test_sblgnt.py
+++ src/tests/test_sblgnt.py
@@ -1,5 +1,8 @@
 import pytest
+from grarticle import sblgnt
 
 def test_parse():
-    pass
-
+    filepath = 'data/sblgnt/61-Mt-morphgnt.txt'
+    parsed = sblgnt.parse(filepath)
+    assert parsed is not None
+    assert len(parsed) > 1
